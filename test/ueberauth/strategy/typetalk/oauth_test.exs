54cc8ce28d2b6d3bd3f535c88c56adb09d8ecba9
Issei Horie <is2ei.horie@gmail.com>
2019-02-11 19:46:08 +0900

    Improve tests

--- test/ueberauth/strategy/typetalk/oauth_test.exs
+++ test/ueberauth/strategy/typetalk/oauth_test.exs
@@ -3,7 +3,25 @@ defmodule Ueberauth.Strategy.Typetalk.OAuthTest do
 
   doctest Ueberauth.Strategy.Typetalk.OAuth
 
-  test "is Truth" do
-    assert 1 + 1 == 2
+  alias Ueberauth.Strategy.Typetalk.OAuth
+
+  setup do
+    Application.put_env(:ueberauth, OAuth,
+      client_id: "CLIENT_ID",
+      client_secret: "CLIENT_SECRET"
+    )
+
+    :ok
+  end
+
+  test "creates correct client" do
+    client = OAuth.client()
+
+    assert client.client_id == "CLIENT_ID"
+    assert client.client_secret == "CLIENT_SECRET"
+
+    assert client.site == "https://typetalk.com"
+    assert client.authorize_url == "/oauth2/authorize"
+    assert client.token_url == "/oauth2/access_token"
   end
 end
